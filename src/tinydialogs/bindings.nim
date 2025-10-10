f81c67f7db6f4211acb62221ad1336dbd1143c2e
Cristobal <cristobalriaga@gmail.com>
2025-10-10 14:37:52 +0200

    rebuilt bindings from nim 2.2.4

--- src/tinydialogs/bindings.nim
+++ src/tinydialogs/bindings.nim
@@ -1,137 +1,167 @@
+{.warning[UnusedImport]: off.}
+{.hint[XDeclaredButNotUsed]: off.}
+from macros import hint, warning, newLit, getSize
 
-from macros import hint
+from os import parentDir
 
-when not declared(tinyfdverbose):
-  var tinyfdverbose*: cint
+when not declared(ownSizeOf):
+  macro ownSizeof(x: typed): untyped =
+    newLit(x.getSize)
+
+when not declared(tinyfd_getGlobalChar):
+  proc tinyfd_getGlobalChar*(
+    aCharVariableName: cstring
+  ): cstring {.cdecl, importc: "tinyfd_getGlobalChar".}
+
 else:
-  static :
-    hint("Declaration of " & "tinyfd_verbose" &
-        " already exists, not redeclaring")
-when not declared(tinyfdassumegraphicdisplay):
-  var tinyfdassumegraphicdisplay*: cint
+  static:
+    hint(
+      "Declaration of " & "tinyfd_getGlobalChar" & " already exists, not redeclaring"
+    )
+when not declared(tinyfd_getGlobalInt):
+  proc tinyfd_getGlobalInt*(
+    aIntVariableName: cstring
+  ): cint {.cdecl, importc: "tinyfd_getGlobalInt".}
+
 else:
-  static :
-    hint("Declaration of " & "tinyfd_assumeGraphicDisplay" &
-        " already exists, not redeclaring")
-when not declared(tinyfdcolorchooser):
-  proc tinyfdcolorchooser*(atitle: cstring; adefaulthexrgb: cstring;
-                           adefaultrgb: array[3'i64, cuchar];
-                           aoresultrgb: array[3'i64, cuchar]): cstring {.cdecl,
-      importc: "tinyfd_colorChooser".}
+  static:
+    hint("Declaration of " & "tinyfd_getGlobalInt" & " already exists, not redeclaring")
+when not declared(tinyfd_setGlobalInt):
+  proc tinyfd_setGlobalInt*(
+    aIntVariableName: cstring, aValue: cint
+  ): cint {.cdecl, importc: "tinyfd_setGlobalInt".}
+
 else:
-  static :
-    hint("Declaration of " & "tinyfd_colorChooser" &
-        " already exists, not redeclaring")
-when not declared(tinyfdresponse):
-  var tinyfdresponse*: array[1024'i64, cschar]
+  static:
+    hint("Declaration of " & "tinyfd_setGlobalInt" & " already exists, not redeclaring")
+when not declared(tinyfd_version):
+  var tinyfd_version* {.importc: "tinyfd_version".}: array[8'i64, cschar]
 else:
-  static :
-    hint("Declaration of " & "tinyfd_response" &
-        " already exists, not redeclaring")
-when not declared(tinyfdsavefiledialog):
-  proc tinyfdsavefiledialog*(atitle: cstring; adefaultpathandfile: cstring;
-                             anumoffilterpatterns: cint;
-                             afilterpatterns: ptr ptr cschar;
-                             asinglefilterdescription: cstring): cstring {.
-      cdecl, importc: "tinyfd_saveFileDialog".}
+  static:
+    hint("Declaration of " & "tinyfd_version" & " already exists, not redeclaring")
+when not declared(tinyfd_needs):
+  var tinyfd_needs* {.importc: "tinyfd_needs".}: ptr UncheckedArray[cschar]
 else:
-  static :
-    hint("Declaration of " & "tinyfd_saveFileDialog" &
-        " already exists, not redeclaring")
-when not declared(tinyfdsilent):
-  var tinyfdsilent*: cint
-else:
-  static :
-    hint("Declaration of " & "tinyfd_silent" &
-        " already exists, not redeclaring")
-when not declared(tinyfdselectfolderdialog):
-  proc tinyfdselectfolderdialog*(atitle: cstring; adefaultpath: cstring): cstring {.
-      cdecl, importc: "tinyfd_selectFolderDialog".}
-else:
-  static :
-    hint("Declaration of " & "tinyfd_selectFolderDialog" &
-        " already exists, not redeclaring")
-when not declared(tinyfdgetglobalint):
-  proc tinyfdgetglobalint*(aintvariablename: cstring): cint {.cdecl,
-      importc: "tinyfd_getGlobalInt".}
-else:
-  static :
-    hint("Declaration of " & "tinyfd_getGlobalInt" &
-        " already exists, not redeclaring")
-when not declared(tinyfdbeep):
-  proc tinyfdbeep*(): void {.cdecl, importc: "tinyfd_beep".}
-else:
-  static :
-    hint("Declaration of " & "tinyfd_beep" & " already exists, not redeclaring")
-when not declared(tinyfdneeds):
-  var tinyfdneeds*: ptr UncheckedArray[cschar]
-else:
-  static :
+  static:
     hint("Declaration of " & "tinyfd_needs" & " already exists, not redeclaring")
-when not declared(tinyfdinputbox):
-  proc tinyfdinputbox*(atitle: cstring; amessage: cstring;
-                       adefaultinput: cstring): cstring {.cdecl,
-      importc: "tinyfd_inputBox".}
+when not declared(tinyfd_verbose):
+  var tinyfd_verbose* {.importc: "tinyfd_verbose".}: cint
 else:
-  static :
-    hint("Declaration of " & "tinyfd_inputBox" &
-        " already exists, not redeclaring")
-when not declared(tinyfdallowcursesdialogs):
-  var tinyfdallowcursesdialogs*: cint
+  static:
+    hint("Declaration of " & "tinyfd_verbose" & " already exists, not redeclaring")
+when not declared(tinyfd_silent):
+  var tinyfd_silent* {.importc: "tinyfd_silent".}: cint
 else:
-  static :
-    hint("Declaration of " & "tinyfd_allowCursesDialogs" &
-        " already exists, not redeclaring")
-when not declared(tinyfdnotifypopup):
-  proc tinyfdnotifypopup*(atitle: cstring; amessage: cstring; aicontype: cstring): cint {.
-      cdecl, importc: "tinyfd_notifyPopup".}
+  static:
+    hint("Declaration of " & "tinyfd_silent" & " already exists, not redeclaring")
+when not declared(tinyfd_allowCursesDialogs):
+  var tinyfd_allowCursesDialogs* {.importc: "tinyfd_allowCursesDialogs".}: cint
 else:
-  static :
-    hint("Declaration of " & "tinyfd_notifyPopup" &
-        " already exists, not redeclaring")
-when not declared(tinyfdversion):
-  var tinyfdversion*: array[8'i64, cschar]
+  static:
+    hint(
+      "Declaration of " & "tinyfd_allowCursesDialogs" &
+        " already exists, not redeclaring"
+    )
+when not declared(tinyfd_forceConsole):
+  var tinyfd_forceConsole* {.importc: "tinyfd_forceConsole".}: cint
 else:
-  static :
-    hint("Declaration of " & "tinyfd_version" &
-        " already exists, not redeclaring")
-when not declared(tinyfdsetglobalint):
-  proc tinyfdsetglobalint*(aintvariablename: cstring; avalue: cint): cint {.
-      cdecl, importc: "tinyfd_setGlobalInt".}
+  static:
+    hint("Declaration of " & "tinyfd_forceConsole" & " already exists, not redeclaring")
+when not declared(tinyfd_assumeGraphicDisplay):
+  var tinyfd_assumeGraphicDisplay* {.importc: "tinyfd_assumeGraphicDisplay".}: cint
 else:
-  static :
-    hint("Declaration of " & "tinyfd_setGlobalInt" &
-        " already exists, not redeclaring")
-when not declared(tinyfdforceconsole):
-  var tinyfdforceconsole*: cint
+  static:
+    hint(
+      "Declaration of " & "tinyfd_assumeGraphicDisplay" &
+        " already exists, not redeclaring"
+    )
+when not declared(tinyfd_response):
+  var tinyfd_response* {.importc: "tinyfd_response".}: array[1024'i64, cschar]
 else:
-  static :
-    hint("Declaration of " & "tinyfd_forceConsole" &
-        " already exists, not redeclaring")
-when not declared(tinyfdmessagebox):
-  proc tinyfdmessagebox*(atitle: cstring; amessage: cstring;
-                         adialogtype: cstring; aicontype: cstring;
-                         adefaultbutton: cint): cint {.cdecl,
-      importc: "tinyfd_messageBox".}
+  static:
+    hint("Declaration of " & "tinyfd_response" & " already exists, not redeclaring")
+when not declared(tinyfd_beep):
+  proc tinyfd_beep*(): void {.cdecl, importc: "tinyfd_beep".}
 else:
-  static :
-    hint("Declaration of " & "tinyfd_messageBox" &
-        " already exists, not redeclaring")
-when not declared(tinyfdopenfiledialog):
-  proc tinyfdopenfiledialog*(atitle: cstring; adefaultpathandfile: cstring;
-                             anumoffilterpatterns: cint;
-                             afilterpatterns: ptr ptr cschar;
-                             asinglefilterdescription: cstring;
-                             aallowmultipleselects: cint): cstring {.cdecl,
-      importc: "tinyfd_openFileDialog".}
+  static:
+    hint("Declaration of " & "tinyfd_beep" & " already exists, not redeclaring")
+when not declared(tinyfd_notifyPopup):
+  proc tinyfd_notifyPopup*(
+    aTitle: cstring, aMessage: cstring, aIconType: cstring
+  ): cint {.cdecl, importc: "tinyfd_notifyPopup".}
+
 else:
-  static :
-    hint("Declaration of " & "tinyfd_openFileDialog" &
-        " already exists, not redeclaring")
-when not declared(tinyfdgetglobalchar):
-  proc tinyfdgetglobalchar*(acharvariablename: cstring): cstring {.cdecl,
-      importc: "tinyfd_getGlobalChar".}
+  static:
+    hint("Declaration of " & "tinyfd_notifyPopup" & " already exists, not redeclaring")
+when not declared(tinyfd_messageBox):
+  proc tinyfd_messageBox*(
+    aTitle: cstring,
+    aMessage: cstring,
+    aDialogType: cstring,
+    aIconType: cstring,
+    aDefaultButton: cint,
+  ): cint {.cdecl, importc: "tinyfd_messageBox".}
+
 else:
-  static :
-    hint("Declaration of " & "tinyfd_getGlobalChar" &
-        " already exists, not redeclaring")
+  static:
+    hint("Declaration of " & "tinyfd_messageBox" & " already exists, not redeclaring")
+when not declared(tinyfd_inputBox):
+  proc tinyfd_inputBox*(
+    aTitle: cstring, aMessage: cstring, aDefaultInput: cstring
+  ): cstring {.cdecl, importc: "tinyfd_inputBox".}
+
+else:
+  static:
+    hint("Declaration of " & "tinyfd_inputBox" & " already exists, not redeclaring")
+when not declared(tinyfd_saveFileDialog):
+  proc tinyfd_saveFileDialog*(
+    aTitle: cstring,
+    aDefaultPathAndFile: cstring,
+    aNumOfFilterPatterns: cint,
+    aFilterPatterns: ptr cstring,
+    aSingleFilterDescription: cstring,
+  ): cstring {.cdecl, importc: "tinyfd_saveFileDialog".}
+
+else:
+  static:
+    hint(
+      "Declaration of " & "tinyfd_saveFileDialog" & " already exists, not redeclaring"
+    )
+when not declared(tinyfd_openFileDialog):
+  proc tinyfd_openFileDialog*(
+    aTitle: cstring,
+    aDefaultPathAndFile: cstring,
+    aNumOfFilterPatterns: cint,
+    aFilterPatterns: ptr cstring,
+    aSingleFilterDescription: cstring,
+    aAllowMultipleSelects: cint,
+  ): cstring {.cdecl, importc: "tinyfd_openFileDialog".}
+
+else:
+  static:
+    hint(
+      "Declaration of " & "tinyfd_openFileDialog" & " already exists, not redeclaring"
+    )
+when not declared(tinyfd_selectFolderDialog):
+  proc tinyfd_selectFolderDialog*(
+    aTitle: cstring, aDefaultPath: cstring
+  ): cstring {.cdecl, importc: "tinyfd_selectFolderDialog".}
+
+else:
+  static:
+    hint(
+      "Declaration of " & "tinyfd_selectFolderDialog" &
+        " already exists, not redeclaring"
+    )
+when not declared(tinyfd_colorChooser):
+  proc tinyfd_colorChooser*(
+    aTitle: cstring,
+    aDefaultHexRGB: cstring,
+    aDefaultRGB: array[3'i64, uint8],
+    aoResultRGB: array[3'i64, uint8],
+  ): cstring {.cdecl, importc: "tinyfd_colorChooser".}
+
+else:
+  static:
+    hint("Declaration of " & "tinyfd_colorChooser" & " already exists, not redeclaring")
+
--- tinydialogs.nimble
+++ tinydialogs.nimble
@@ -1,11 +1,11 @@
 # Package
 
-version       = "1.1.0"
-author        = "Patitotective"
-description   = "Tiny file dialogs bindings for Nim"
-license       = "MIT"
-srcDir        = "src"
-skipFiles     = @["tinydialogs/gen.nim"]
+version = "1.1.1"
+author = "Patitotective"
+description = "Tiny file dialogs bindings for Nim"
+license = "MIT"
+srcDir = "src"
+skipFiles = @["tinydialogs/gen.nim"]
 
 # Dependencies
 
